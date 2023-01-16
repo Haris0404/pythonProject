63bb5b2ba1776d25b992cdaa8fb59f331ec671bd
Haris0404 <118525649+Haris0404@users.noreply.github.com>
2023-01-16 09:17:15 +0100

    Add files via upload

---
+++ TP4 Ex1.py
@@ -0,0 +1,6 @@
+x=float(input("Vous cherchez la table de multiplication de quel nombre ?"))
+liste = [0,1,2,3,4,5,6,7,8,9]
+for i in range (0,9):
+    round(x)
+    print(x,"*",liste[i],"=",x*liste[i],)
+
