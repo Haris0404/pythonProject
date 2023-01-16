ed664061b14c3c85bf36e302f364361312a2834b
Haris0404 <118525649+Haris0404@users.noreply.github.com>
2023-01-16 10:41:24 +0100

    Add files via upload

---
+++ ex7 tp4.py
@@ -0,0 +1,6 @@
+login1 = input("Entrez votre login : ")
+login2 = input("Entrez le login de votre voisin de TP : ")
+
+binome = (login1, login2)
+
+print("L'étudiant " + binome[0] + " est en binôme avec l'étudiant " + binome[1])
